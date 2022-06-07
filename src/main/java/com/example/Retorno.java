8e615a3c2e0c63c34953c896116a018d8fee7f90
Jun23333333 <106772376+Jun23333333@users.noreply.github.com>
2022-06-06 20:39:15 -0500

    Add files via upload
    problema 3

---
+++ src/main/java/com/example/Retorno.java
@@ -0,0 +1,38 @@
+package com.example;
+
+public class Retorno {
+    public void retornar(String nombre, String apellido){
+        int a = nombre.length();
+        int b = apellido.length();
+        String nombre_retornado = "a", apellido_retornado = "a";
+        int x = 1;
+        int y = 1;
+        while (x <= a){
+            if(x == 1){
+                char primera_letra = nombre.charAt(1);
+                nombre_retornado = String.valueOf(Character.toUpperCase(primera_letra));
+            }
+            else{
+                char letra = nombre.charAt(x);
+                nombre_retornado = String.format(nombre_retornado + String.valueOf(Character.toLowerCase(letra)));
+
+            }
+            x ++;
+        }
+        while (!(y > b)){
+            if(1 == y){
+                char primera_letra = apellido.charAt(1);
+                apellido_retornado = String.valueOf(Character.toUpperCase(primera_letra));
+            }
+            else{
+                char letra = apellido.charAt(x);
+                apellido_retornado = String.format(apellido_retornado + String.valueOf(Character.toLowerCase(letra)));
+
+            }
+            y ++;
+        }
+
+    }
+
+
+}
